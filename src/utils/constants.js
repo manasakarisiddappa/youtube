f568afaf587d7b5d92d7f8f9fb8d12c61897d10e
Manasa <mail2manasak@gmail.com>
2024-05-28 10:55:59 +0530

    enviromment file added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyAPkCjf7AY1WOm5-O4gQrZYozsxoDISAJw";
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YOUTUBE_API_KEY =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
